fd2edda22e428cbe812046a865db6498c95d45d4
Jorge Arancibia <jorge.arancibia.jeal@gmail.com>
2020-01-28 20:28:48 -0300

    optimization

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -5,7 +5,6 @@ const Certifications = props => {
   return (
     <div className="d-flex flex-sm-nowrap flex-md-nowrap flex-lg-nowrap certificationBox">
       <CertificationsModel {...props.data.info[6]}/>
-      <CertificationsModel {...props.data.info[7]} space />
     </div>
   )
 }
